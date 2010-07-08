90c23ff55623aa1b01362c91169d73a6b9476950
Lee Packham <lpackham@leenux.org.uk>
2010-07-08 10:18:09 +0100

    Add mercurial to the GIT prompt - still want it.

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,5 +1,14 @@
+function hg_prompt_info {
+    hg prompt --angle-brackets "\
+< on %{$fg[magenta]%}<branch>%{$reset_color%}>\
+< at %{$fg[yellow]%}<tags|%{$reset_color%}, %{$fg[yellow]%}>%{$reset_color%}>\
+%{$fg[green]%}<status|modified|unknown><update>%{$reset_color%}<
+patches: <patches|join( → )|pre_applied(%{$fg[yellow]%})|post_applied(%{$reset_color%})|pre_unapplied(%{$fg_bold[black]%})|post_unapplied(%{$reset_color%})>>" 2>/dev/null
+}
+
 # get the name of the branch we are on
 function git_prompt_info() {
+  echo "$(hg_prompt_info)"
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
